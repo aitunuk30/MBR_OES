47952a39b8e58c874d46912a6ef62cc3f40da9f8
aitunuk30 <aitunukbaktybekova.007bka@gmail.com>
2023-12-18 16:33:56 +0600

    Add Passing marks for Exams

--- resources/views/admin/marksDashboard.blade.php
+++ resources/views/admin/marksDashboard.blade.php
@@ -10,6 +10,7 @@
         <th>Exam Name</th>
         <th>Marks/Q</th>
         <th>Total Marks</th>
+        <th>Passing Marks</th>
         <th>Edit</th>
 
 
@@ -25,8 +26,9 @@
                   <td>{{ $exam-> exam_name }}</td>
                   <td>{{ $exam-> marks }}</td>
                   <td>{{ count($exam-> getQnaExam) * $exam-> marks }}</td>
+                  <td>{{ $exam->pass_marks }}</td>
                   <td>
-                      <button class= "btn btn-primary editMarks" data-id = "{{$exam->id}}" data-marks="{{ $exam->marks}}"data-totalq="{{count($exam-> getQnaExam)}}"  data-toggle="modal" data-target="#editMarksModal">Edit</button>
+                      <button class= "btn btn-primary editMarks" data-id = "{{$exam->id}}"  data-pass-marks="{{ $exam->pass_marks }}" data-marks="{{ $exam->marks}}"data-totalq="{{count($exam-> getQnaExam)}}"  data-toggle="modal" data-target="#editMarksModal">Edit</button>
                   </td>
               </tr>
               @endforeach
@@ -57,7 +59,7 @@
             <div class="modal-body">
 
                 <div class="row">
-                    <div class="col-sm-3">
+                    <div class="col-sm-4">
                         <label>Marks/Q</label>
 
                     </div>
@@ -70,16 +72,24 @@
                     </div>
                 </div>
 
-                   <div class="row">
-                    <div class="col-sm-3">
-                        <label>Total Marks</label>
-
+                   <div class="row mt-2">
+                        <div class="col-sm-4">
+                            <label>Total Marks</label>
+                        </div>
+                        <div class="col-sm-6">
+                               <input type="text" placeholder="Total Marks" id="tmarks">
+                        </div>
                     </div>
-                    <div class="col-sm-6">
 
-                        <input type="text" placeholder="Total Marks" id="tmarks">
-
-                    </div>
+                    <div class="row mt-2">
+                        <div class="col-sm-4">
+                              <label>Passing Marks</label>
+                        </div>
+                        <div class="col-sm-6">
+                                 <input type="text"
+                                    onkeypress="return event.charCode>=48 && event.charCode<=57 || event.charCode == 46"
+                                   name="pass_marks" placeholder="Enter Passing Marks" id="pass_marks" required>
+                        </div>
                 </div>
             </div>
 
@@ -112,14 +122,52 @@
 
             totalQna = totalq;
 
+            $('#pass_marks').val($(this).attr('data-pass-marks'));
+
         });
         $('#marks').keyup(function() {
             $('#tmarks').val( ($(this).val()*totalQna).toFixed(1) );
         });
 
+
+
+        $('#pass_marks').keyup(function() {
+            $('.pass_error').remove();
+            var tmarks = $('#tmarks').val();
+            var pmarks = $(this).val();
+
+            if (parseFloat(pmarks) >= parseFloat(tmarks)) {
+                $(this).parent().append('<p style= "color:red;" class ="pass-error" >Passing Marks will be less than total marks!</p>');
+                setTimeout(()=> {
+                    $('.pass-error').remove();
+
+                }, 2000);
+
+
+            }
+
+
+        });
+
         $('#editMarks').submit(function(event) {
 
             event.preventDefault();
+
+            $('.pass-error').remove();
+            var tmarks = $('#tmarks').val();
+            var pmarks = $('#pass_marks').val();
+
+            if (parseFloat(pmarks) >= parseFloat(tmarks)) {
+                $('#pass_marks').parent().append('<p style= "color:red;" class ="pass-error" >Passing Marks will be less than total marks!</p>');
+                setTimeout(()=> {
+                    $('.pass-error').remove();
+
+                }, 2000);
+
+                return false;
+
+
+            }
             var formData = $(this).serialize();
             $.ajax({
 
